952be5819c82c1743b6929de004fb3458fdcb638
gamadv <lealdev.py@gmail.com>
2022-06-13 20:04:27 -0300

    feat: New components inserted; global styles added; create App Styles

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+
+import styles from './App.module.css'
+import './global.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          posts here
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
