abb0012386559013245a81fd21a487c554b6a342
gamadv <lealdev.py@gmail.com>
2022-06-13 20:03:24 -0300

    feat: Header component created;

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import style from './styles.module.css'
+
+import igniteLogo from '../../assets/ignite-logo.svg'
+
+export default function Header() {
+
+    return (
+        <header className={style.header}>
+            <img src={igniteLogo} alt="Ignite Logo" height="60px" />
+            <h1>My Ignite Feed</h1>
+        </header>
+    )
+}
