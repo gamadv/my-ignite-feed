3a6262acea37fbecc53d8c6016b96ad49e0dac09
gamadv <lealdev.py@gmail.com>
2022-06-14 18:34:46 -0300

    fix: Export default changed to Named Export; And Avatar component inserted

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,17 +1,18 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from '../Avatar'
 
 import styles from './styles.module.css'
 
-export default function Sidebar() {
+export function Sidebar() {
 
     const imgUrl = 'https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40'
-    const profileImg = 'https://github.com/gamadv.png'
+    const profileImgURL = 'https://github.com/gamadv.png'
 
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src={imgUrl} alt="User Background" />
             <div className={styles.profile}>
-                <img className={styles.avatar} src={profileImg} alt="Avatar Image" />
+                <Avatar src={profileImgURL} />
                 <strong> Moacir Gama </strong>
                 <span>Frontend Developer</span>
             </div>
